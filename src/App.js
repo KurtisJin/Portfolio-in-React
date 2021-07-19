6c8cd792af781960bf70c1a2afd649fc98390240
Kurtis Jin <cloudnine21@gmail.com>
2021-07-18 23:07:32 -0500

    added contact me and footer. Still need to work on navbar sticky and scolly and carousel

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import './App.css';
 import { Navbar } from './components/Navigation/Navbar';
 import LandingPage from './components/LandingPage/LandingPage'
 import AboutMe from './components/AboutMe/AboutMe'
-import ScrollReact from './components/ScrollUpButton/ScrollReact';
+// import ScrollReact from './components/ScrollUpButton/ScrollReact';
 import Services from './components/Services/Services'
 import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
-
+import ContactMe from './components/ContactMe/Contactme'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-      <ScrollReact/>
+      {/* <ScrollReact/> */}
       <Navbar/>
       <LandingPage/>
       <AboutMe/>
       <Services/>
       <Skills/>
       <Projects/>
+      <ContactMe/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+// import OwlCarousel from 'react-owl-carousel2';
+// import 'react-owl-carousel2/lib/styles.css';
+// import 'react-owl-carousel2/src/owl.theme.default.css';
 
 export default function Projects() {
-
+    const options = {
+        items: 1,
+        nav: true,
+        rewind: true,
+        autoplay: true
+    };
    
 
     return (
@@ -12,44 +17,44 @@ export default function Projects() {
         <section className="projects" id="projects">
             <div className="max-width">
                 <h2 className="title">Projects</h2>
-                <OwlCarousel>
-                <div className="carousel owl-carousel">
-                    <div className="card">
-                        <div className="box">
-                        <a href="https://kurtisjin.github.io/Movie-Application/"><img src="./Assets/images/project1.JPG" alt=""/></a>
-                        <div className="text"><a href="https://kurtisjin.github.io/Movie-Application/">Movie Search</a></div>
+                {/* <OwlCarousel ref="car" options={options}> */}
+                    <div className="carousel owl-carousel">
+                        <div className="card">
+                            <div className="box">
+                            <a href="https://kurtisjin.github.io/Movie-Application/"><img src="./Assets/images/project1.JPG" alt=""/></a>
+                            <div className="text"><a href="https://kurtisjin.github.io/Movie-Application/">Movie Search</a></div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="box">
+                                <a href="https://kurtisjin.github.io/Password-Generator/"><img src="./Assets/images/password_generator.JPG" alt="" /></a>
+                                <div className="text"><a href="https://kurtisjin.github.io/Password-Generator/">Password Generator</a></div>
+                                <p>Random Password Generator. Answer prompts to create your password. <br></br> <br></br> Github Information: <a href="https://github.com/KurtisJin/Random-Password-Generator">Click Here</a></p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="box">
+                                <img src="./Assets/images/weather_app.JPG" alt="" />
+                                <div className="text"><a href="https://kurtisjin.github.io/Weather-Application/">Weather Application</a></div>
+                                <p>Type in your city to pull up weather information. <br></br><br></br><br></br> Github Information: <a href="https://github.com/KurtisJin/Weather-Application">Click Here</a></p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="box">
+                                <img src="./Assets/images/boundman2.JPG" alt="picture of luffy animation gear 4 in fighting stance to unleash hell" />
+                                <div className="text"><a href="https://kurtisjin.github.io/One-Piece-Quiz/">One Piece Quiz!</a></div>
+                                <p>One Piece Quiz. If you are right, the background will be gree. If wrong, it will be blue! Beat the timer!<br></br><br></br> Github Information: <a href="https://github.com/KurtisJin/One-Piece-Quiz/settings">Click Here</a></p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="box">
+                                <img src="./Assets/images/workday_scheduler.JPG" alt="picture of scheduler application. Save button icon. work day hours" />
+                                <div className="text"><a href="https://kurtisjin.github.io/Work-Day-Scheduler/">Work day Scheduler</a></div>
+                                <p>Schedule your workday with the scheduler application!<br></br><br></br><br></br> Github Information: <a href="https://github.com/KurtisJin/Work-Day-Scheduler">Click Here</a></p>
+                            </div>
                         </div>
                     </div>
-                    <div className="card">
-                        <div className="box">
-                            <a href="https://kurtisjin.github.io/Password-Generator/"><img src="./Assets/images/password_generator.JPG" alt="" /></a>
-                            <div className="text"><a href="https://kurtisjin.github.io/Password-Generator/">Password Generator</a></div>
-                            <p>Random Password Generator. Answer prompts to create your password. <br></br> <br></br> Github Information: <a href="https://github.com/KurtisJin/Random-Password-Generator">Click Here</a></p>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="box">
-                            <img src="./Assets/images/weather_app.JPG" alt="" />
-                            <div className="text"><a href="https://kurtisjin.github.io/Weather-Application/">Weather Application</a></div>
-                            <p>Type in your city to pull up weather information. <br></br><br></br><br></br> Github Information: <a href="https://github.com/KurtisJin/Weather-Application">Click Here</a></p>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="box">
-                            <img src="./Assets/images/boundman2.JPG" alt="picture of luffy animation gear 4 in fighting stance to unleash hell" />
-                            <div className="text"><a href="https://kurtisjin.github.io/One-Piece-Quiz/">One Piece Quiz!</a></div>
-                            <p>One Piece Quiz. If you are right, the background will be gree. If wrong, it will be blue! Beat the timer!<br></br><br></br> Github Information: <a href="https://github.com/KurtisJin/One-Piece-Quiz/settings">Click Here</a></p>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="box">
-                            <img src="./Assets/images/workday_scheduler.JPG" alt="picture of scheduler application. Save button icon. work day hours" />
-                            <div className="text"><a href="https://kurtisjin.github.io/Work-Day-Scheduler/">Work day Scheduler</a></div>
-                            <p>Schedule your workday with the scheduler application!<br></br><br></br><br></br> Github Information: <a href="https://github.com/KurtisJin/Work-Day-Scheduler">Click Here</a></p>
-                        </div>
-                    </div>
-                </div>
-                </OwlCarousel> 
+                {/* </OwlCarousel>  */}
             </div>
 
         </section>
